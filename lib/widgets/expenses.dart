13267e9be69a3da698358d9869ab746127b55336
Aditya <yendralwaraditya@gmail.com>
2023-07-04 02:05:04 +0530

    created expenses list widget and added listview to it

---
+++ lib/widgets/expenses.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:my_expenses/models/expense.dart';
+import 'package:my_expenses/widgets/expenses_list.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> registeredExpenses = [
+    Expense(
+      title: 'Food',
+      amount: 50,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+    Expense(
+      title: 'Cigarette',
+      amount: 18,
+      date: DateTime.now(),
+      category: Category.cigarette,
+    ),
+    Expense(
+      title: 'Movie',
+      amount: 200,
+      date: DateTime.now(),
+      category: Category.movie,
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          const Text('Chart'),
+          Expanded(
+            child: ExpensesList(
+              expenses: registeredExpenses,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
